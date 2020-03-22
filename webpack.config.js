b36befccc62d59d7e404b7b564a7f037d09f215d
okonomi <okonomi@oknm.jp>
2020-03-22 15:29:01 +0900

    build: compile typescript with webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,24 @@
 const path = require('path')
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './src/index.ts',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'public/dist')
+  },
+  resolve: {
+    extensions: [
+      '.ts',
+      '.js'
+    ]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        loader: 'ts-loader'
+      }
+    ]
   }
 }
